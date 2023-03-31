f8e7660a38feace3cdbfb67983238aa3c3cffa69
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-03-31 08:25:56 +0200

    added new custom domain for megalopolik

--- backend/util/liveGames.ts
+++ backend/util/liveGames.ts
@@ -3,7 +3,8 @@ import webHookBody from "../@types/webhook";
 let liveGames = new Map<string, webHookBody>
 let redirectUrl: Map<string, string> = new Map([
     ["4967963b-566f-4e8a-908b-02c8315d12f1","http://edox.faceitlobby.com"],
-    ["afce1ec2-1b60-47bd-88c1-bfadce96876a", "http://danielpello.faceitlobby.com"]
+    ["afce1ec2-1b60-47bd-88c1-bfadce96876a", "http://danielpello.faceitlobby.com"],
+    ["e3cefc97-36b1-4615-91d6-cdcca13e4e58","http://megalopolik.faceitlobby.com"]
 ]
    
 );
