9325aaf7a5338f57d8051a2a7a7dc8ff3a101ecb
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-04-19 13:01:24 +0200

    fixed wingman crash

--- backend/server/routes/legacy/oldHeroku.post.ts
+++ backend/server/routes/legacy/oldHeroku.post.ts
@@ -1,11 +1,11 @@
 import { Router, type Request, type Response } from 'express'
 import { addMatchroomToDB } from '../../../util/database/addToDatabase'
-import { addEloInformation, validateAuthorizationHeader, validateEvent, validateStreamerGame } from '../../middleware/match'
+import { addEloInformation, validateAuthorizationHeader, validateEvent, validateStreamerGame, validateGameMode } from '../../middleware/match'
 import { addGame, getGame } from '../../../util/liveGames'
 
 const legacyPostRouter: Router = Router()
 
-legacyPostRouter.post('/match',validateAuthorizationHeader,validateEvent, validateStreamerGame, addEloInformation, (req: Request, res: Response) => {
+legacyPostRouter.post('/match',validateAuthorizationHeader,validateGameMode,validateEvent, validateStreamerGame, addEloInformation, (req: Request, res: Response) => {
   addGame(req.headers.authorization as string, req.body)
   addMatchroomToDB(req.body)
   res.json(getGame(req.headers.authorization as string))
