46497bab002ce1ad49b4bb531fa249a747964fd1
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-10-11 12:39:09 +0200

    removed index route

--- backend/index.ts
+++ backend/index.ts
@@ -9,4 +9,5 @@ getLiveGames().then(games =>{
     console.log(games.length)
     initLiveGames(games)
 })
+console.log("This has been updated")
 server.listen(SECRETS.PORT, () => { console.log(`Server In Container Listening On Port: ${SECRETS.PORT}`) })
