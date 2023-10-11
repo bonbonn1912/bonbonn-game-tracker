41ba01fc8b6444b7ab19d7f83034b4cfc9131897
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-10-11 12:18:27 +0200

    added post test route

--- backend/server/routes/default.ts
+++ backend/server/routes/default.ts
@@ -7,6 +7,10 @@ defaultRouter.get('/test', (req: Request, res: Response) => {
   res.send({ message: 'Hello World!' })
 })
 
+defaultRouter.post('/test', (req: Request, res: Response) => {
+  res.send({ message: 'Hello World!' })
+})
+
 /**
  * ts-node needs another static path since the application doesnt run from build (frontend needs to be build before)
  */
