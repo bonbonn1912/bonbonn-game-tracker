12e5ef61678fe9907e9ad654e54132e640d6a025
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2025-01-17 15:47:05 +0100

    Added Players

--- backend/util/liveGames.ts
+++ backend/util/liveGames.ts
@@ -6,7 +6,9 @@ let redirectUrl: Map<string, string> = new Map([
     ["afce1ec2-1b60-47bd-88c1-bfadce96876a", "http://danielpello.faceitlobby.com"],
     ["e3cefc97-36b1-4615-91d6-cdcca13e4e58","http://megalopolik.faceitlobby.com"],
     ["f335cfd1-3a92-4365-9b25-2f0e82a6052f","http://mrc9cs.faceitlobby.com"],
-     ["e64b6d73-6313-4527-b313-a852e6368cf2","http://redcs.faceitlobby.com"]
+     ["e64b6d73-6313-4527-b313-a852e6368cf2","http://redcs.faceitlobby.com"],
+     ["c77a24f9-4126-4321-9cc5-4dfcf69b50ee","http://bulmi.faceitlobby.com"],
+     ["f8bd2fd4-1860-441e-8851-3469dd3a1a55","http://tiziaN.faceitlobby.com"],
  //   ["179d5eb7-be1a-473a-8bc5-36310ebc0f28","http://missmoeppi.faceitlobby.com"]
 ]
    
